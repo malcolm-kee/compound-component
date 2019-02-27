97641c76abc2e551aa45ad08c9bb0b82f882c8e8
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-02-27 19:06:32 +0800

    update initial state

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { Field } from './components/field';
 
 function App() {
+  const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
 
   return (
     <div className="App">
       <div className="container">
         <dl>
+          <dt>name</dt>
+          <dd>{name}</dd>
           <dt>email</dt>
           <dd>{email}</dd>
         </dl>
         <Field
+          label="Name"
+          onChange={ev => setName(ev.target.value)}
+          value={name}
+        />
+        <Field
           label="Email"
           type="email"
+          id="email-control"
           onChange={ev => setEmail(ev.target.value)}
           value={email}
         />
