0a462cc8a53d19da1133bcc8e96882e234bee864
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-02-27 21:21:55 +0800

    complete final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Field } from './components/field';
+import { Field, Label, Input } from './components/field';
+import { Router, Route, Link, Switch } from './components/simple-router';
 
-function App() {
+function HomePage() {
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
 
@@ -14,20 +15,75 @@ function App() {
           <dt>email</dt>
           <dd>{email}</dd>
         </dl>
-        <Field
-          label="Name"
-          onChange={ev => setName(ev.target.value)}
-          value={name}
-        />
-        <Field
-          label="Email"
-          type="email"
-          id="email-control"
-          onChange={ev => setEmail(ev.target.value)}
-          value={email}
-        />
+        <Field>
+          <Label>Name</Label>
+          <Input onChange={ev => setName(ev.target.value)} value={name} />
+        </Field>
+        <Field>
+          <Label>Email</Label>
+          <div className="input-group">
+            <span className="input-group-addon">@</span>
+            <Input
+              type="email"
+              id="email-control"
+              onChange={ev => setEmail(ev.target.value)}
+              value={email}
+            />
+          </div>
+        </Field>
+        <div>
+          <Link to="/two">Page Two</Link> | <Link to="/three">Page Three</Link>{' '}
+          | <Link to="/asdf9">Somewhere not exists</Link>
+        </div>
       </div>
     </div>
+  );
+}
+
+function PageTwo() {
+  return (
+    <div>
+      <h1>Page Two</h1>
+      <div>
+        <Link to="/">Home</Link> | <Link to="/three">Page Three</Link> |{' '}
+        <Link to="/asdf9">Somewhere not exists</Link>
+      </div>
+    </div>
+  );
+}
+
+function PageThree() {
+  return (
+    <div>
+      <h1>Page Three</h1>
+      <div>
+        <Link to="/">Home</Link> | <Link to="/two">Page Two</Link> |{' '}
+        <Link to="/asdf9">Somewhere not exists</Link>
+      </div>
+    </div>
+  );
+}
+
+function ErrorPage() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>The sadness...</p>
+      <Link to="/">Go back to home</Link>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route path="/two" component={PageTwo} />
+        <Route path="/three" component={PageThree} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/field.jsx
+++ src/components/field.jsx
@@ -5,17 +5,38 @@ let id = Date.now();
 
 const getId = () => `field-${id++}`;
 
-export const Field = ({ label, className, id, ...inputProps }) => {
-  const defaultFieldId = React.useRef(getId());
+const FieldContext = React.createContext({
+  inputId: undefined,
+  setInputId: function noop() {}
+});
+
+export const Field = ({ children }) => {
+  const [inputId, setInputId] = React.useState(() => getId());
 
   return (
-      <div className="form-group">
-        {label && <label htmlFor={id || defaultFieldId.current}>{label}</label>}
-        <input 
-          className={classNames('form-control', className)} 
-          id={id || defaultFieldId.current} 
-          {...inputProps} 
-        />
-      </div>
+    <FieldContext.Provider value={{ inputId, setInputId }}>
+      <div className="form-group">{children}</div>
+    </FieldContext.Provider>
+  );
+};
+
+export const Label = props => {
+  const { inputId } = React.useContext(FieldContext);
+  return <label htmlFor={inputId} {...props} />;
+};
+
+export const Input = ({ className, ...inputProps }) => {
+  const { inputId, setInputId } = React.useContext(FieldContext);
+  React.useEffect(() => {
+    if (inputProps.id && inputProps.id !== inputId) {
+      setInputId(inputProps.id);
+    }
+  }, [inputProps.id]);
+  return (
+    <input
+      className={classNames('form-control', className)}
+      id={inputId}
+      {...inputProps}
+    />
   );
 };
